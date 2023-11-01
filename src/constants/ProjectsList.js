89ff571a4c7d0b065d893b5491f5b0b44c3d66f9
vivekdhumal28 <vivekdhumal.28@gmail.com>
2023-11-01 09:37:49 +0530

    Project update

--- src/constants/ProjectsList.js
+++ src/constants/ProjectsList.js
@@ -1,4 +1,5 @@
 import MovieHub from "../img/MoviePoster.png";
+import Draw from "../img/Draw.png";
 
 const projectInfo = [
   {
@@ -12,12 +13,12 @@ const projectInfo = [
   },
   {
     id: 2,
-    portrait: MovieHub,
-    name: "Movie Hub 1",
+    portrait: Draw,
+    name: "Draw App",
     discription:
       " euismod augue at velit volutpat. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sy",
-    github: "https://github.com/vivekdhumal28/movie-hub",
-    visitLink: "https://mymovie-hub.netlify.app/",
+    github: "https://github.com/vivekdhumal28/Draw",
+    visitLink: "https://dedraw.netlify.app/",
   },
 ];
 
